c7c9b3208ec66150e7c88fc1844c9b125468ccdf
HP <abhinavchauhan744@gmail.com>
2025-05-05 21:02:49 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`https://digital-frontend-2vr5.onrender.com`
+        image_url: `https://digital-backend-xyz.onrender.com/images/${req.file.filename}`
     })
 })
 
